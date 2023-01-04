1095cc963e7081afbda25f80fc5c22b7d5f166fc
Manoumey <manuella.meyer@dci.education>
2023-01-04 15:34:42 +0100

    first commit

---
+++ solution.js
@@ -0,0 +1,38 @@
+//1. Welcher Typ?
+
+  //  Prüfe, ob 3 gleich "3" ist, indem du den "einfachen" Vergleich verwendest. Prüfe dann, ob sie gleich sind, indem du den strengen (strict) Vergleich verwendest. Fragen: Gib die Nummer der richtigen Antwort im Terminal aus (eine nach der anderen)
+   // Welche der folgenden Möglichkeiten wird verwendet, um den WERT von zwei Variablen/Werten zu vergleichen?
+
+
+   console.log(3=="3");
+   console.log(3==="3");
+
+// Welche der folgenden Möglichkeiten wird verwendet, um den WERT von zwei Variablen/Werten zu vergleichen?
+   console.log("2");
+
+//Welche der folgenden Möglichkeiten wird verwendet, um den WERT und den TYP von zwei Variablen/Werten zu vergleichen?
+console.log("7");
+
+//Welche der folgenden Möglichkeiten wird verwendet, um einer Variablen einen Wert zuzuweisen?
+console.log("9");
+
+//2. Nicht
+
+  //  Vervollständige den folgenden ternären Code, um "good evening" mit "myVar" auszudrucken:
+
+  let myVar = true;
+
+console.log( myVar=false ? 'good morning' : 'good evening' );
+
+//3. Kurzer Kreislauf
+
+   // Vervollständige den folgenden Code, indem du die Leerräume mit '&&' oder '||' ausfüllst, um den Wert zu erhalten, der zum Namen der Variablen passt: (PS: diese Werte geben immer false zurück: false , 0, '')
+
+let firstName = '' || 'John' || 0
+let emptyStr = '' && false && 'Hello World'
+let zero = '' || false || 0
+let seven = 75 && 'nine' && 7
+
+console.log(firstName,zero,emptyStr,seven);
+
+
